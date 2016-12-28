66a911513bf66289ddad014ea58ab04ef09dee83
Ambroise Couissin <ambroise.couissin@gmail.com>
2016-12-28 15:01:45 +0800

    Little application showing core functionalities (sound levels, haversine, voice power recovery...)

--- Yell.Hosts.ConsoleApp/Program.cs
+++ Yell.Hosts.ConsoleApp/Program.cs
@@ -4,7 +4,11 @@ using Yell.Models;
 
 public class Program
 {
-    private static List<User> Users => new List<User>
+    public static List<User> Users { get; set; }
+
+    public static void Main(string[] args)
+    {
+        Users = new List<User>
         {
             new User
             {
@@ -12,7 +16,7 @@ public class Program
                 UserName = "Bambi",
                 Position = new Tuple<double, double>(22.2863943, 114.14913750000005),
                 //Ear = new Ear { EaringDistance = 400 },
-                Voice = new Voice { RecoveryRate = 0.0001m, MaxPower = 0.001m }
+                Voice = new Voice { RecoveryRate = 0.00003m, MaxPower = 0.001m }
             },
             new User
             {
@@ -20,7 +24,7 @@ public class Program
                 UserName = "Panpan",
                 Position = new Tuple<double, double>(22.2811673, 114.14244899999994),
                 //Ear = new Ear { EaringDistance = 400 },
-                Voice = new Voice { RecoveryRate = 0.0001m, MaxPower = 0.001m }
+                Voice = new Voice { RecoveryRate = 0.00003m, MaxPower = 0.001m }
             },
             new User
             {
@@ -28,35 +32,36 @@ public class Program
                 UserName = "Fleur",
                 Position = new Tuple<double, double>(22.2808373, 114.15567120000003),
                 //Ear = new Ear { EaringDistance = 400 },
-                Voice = new Voice { RecoveryRate = 0.0001m, MaxPower = 0.001m }
+                Voice = new Voice { RecoveryRate = 0.00003m, MaxPower = 0.001m }
             }
         };
 
-    public static void Main(string[] args)
-    {
-        Console.Write("Select a user (1, 2 or 3): ");
-
-        ConsoleKeyInfo key = Console.ReadKey();
-
-        User user = Users.Find(u => u.Id == key.KeyChar.ToString());
-        Console.WriteLine();
-        Console.WriteLine($"Id: {user.Id}, Name: {user.UserName}, Latitude: {user.Position.Item1}, Longitude: {user.Position.Item1}");
-        Console.WriteLine();
-
-        Console.Write("What do you want to yell?: ");
-        string messageContent = Console.ReadLine();
-        Console.WriteLine();
-
-        Console.Write("How loud (in Watt)?: ");       
-        decimal power = Convert.ToDecimal(Console.ReadLine());
-        Console.WriteLine();
-
-        Message message = user.Yell(messageContent, power);
-        foreach (User u in Users)
+        while (true)
         {
-            u.Hear(message);
-        }
+            Console.Write("Select a user (1, 2 or 3): ");
 
-        Console.ReadKey();
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            User user = Users.Find(u => u.Id == key.KeyChar.ToString());
+            Console.WriteLine();
+            Console.WriteLine($"Id: {user.Id}, Name: {user.UserName}, Latitude: {user.Position.Item1}, Longitude: {user.Position.Item1}");
+            Console.WriteLine();
+
+            Console.Write("What do you want to yell?: ");
+            string messageContent = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("How loud (in Watt)?: ");
+            decimal power = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine();
+
+            Message message = user.Yell(messageContent, power);
+            foreach (User u in Users)
+                u.Hear(message);
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
     }
 }
